5a474007c4ea7ca74fb79ec86c680ce475dd7a5b
Steven Enamakel <enamakel@tinyhumans.ai>
2026-06-14 01:40:08 -0700

    feat(website): install favicon and web app manifest

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -38,6 +38,18 @@ export const metadata: Metadata = {
 		index: true,
 		follow: true,
 	},
+	icons: {
+		icon: [
+			{ url: "/favicon-96x96.png", type: "image/png", sizes: "96x96" },
+			{ url: "/favicon.svg", type: "image/svg+xml" },
+		],
+		shortcut: "/favicon.ico",
+		apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+	},
+	manifest: "/site.webmanifest",
+	appleWebApp: {
+		title: "Tiny.Place",
+	},
 };
 
 type RootLayoutProperties = {
